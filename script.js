a67ef7d009cde6b1db34388f54316aeee67b2e2e
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2023-09-20 06:58:37 -0700

    started scipt

---
+++ script.js
@@ -0,0 +1,9 @@
+//Tabbed Menu
+function openMenu(event, menuName) {
+    let menuArray = document.getElementsByClassName('menu')
+    for (let i=0; i < menuArray.length; i++ ) {
+        menuArray[i].style.display = 'none'
+    }
+
+    let tablinks = document.getElementsByClassName('menu')
+}
